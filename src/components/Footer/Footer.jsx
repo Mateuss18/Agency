c2e2fbc20900f13a3e3613fc5c15ffb167c60985
MateusLopes <mateus20.lopes02@gmail.com>
2023-02-07 16:34:31 -0300

    feat(footer): add componente footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import './footer.scss'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer-container">
+                <div className="footer-desc-container">
+                    <h3 className="footer-title">Agency</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Tenetur dicta dolorum necessitatibus eum atque iste
+                        quasi suscipit, ad perspiciatis sequi obcaecati,
+                        deserunt adipisci voluptatem dolore blanditiis, odio
+                        tempora maxime explicabo!
+                    </p>
+                </div>
+                <div className="footer-links-container">
+                    <div className="footer-links-container-list">
+                        <p className="footer-title">Link</p>
+                        <ul>
+                            <li>
+                                <a href="#">Base de conhecimentos</a>
+                            </li>
+                            <li>
+                                <a href="#">Trabalhe Conosco</a>
+                            </li>
+                            <li>
+                                <a href="#">Ultimos Projetos</a>
+                            </li>
+                            <li>
+                                <a href="#">Politica de Privacidade</a>
+                            </li>
+                            <li>
+                                <a href="#">Contato</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="footer-links-container-list">
+                        <p className="footer-title">Link</p>
+                        <ul>
+                            <li>
+                                <a href="#">Base de conhecimentos</a>
+                            </li>
+                            <li>
+                                <a href="#">Trabalhe Conosco</a>
+                            </li>
+                            <li>
+                                <a href="#">Ultimos Projetos</a>
+                            </li>
+                            <li>
+                                <a href="#">Politica de Privacidade</a>
+                            </li>
+                            <li>
+                                <a href="#">Contato</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="footer-links-container-list">
+                        <p className="footer-title">Link</p>
+                        <ul>
+                            <li>
+                                <a href="#">Base de conhecimentos</a>
+                            </li>
+                            <li>
+                                <a href="#">Trabalhe Conosco</a>
+                            </li>
+                            <li>
+                                <a href="#">Ultimos Projetos</a>
+                            </li>
+                            <li>
+                                <a href="#">Politica de Privacidade</a>
+                            </li>
+                            <li>
+                                <a href="#">Contato</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="footer-copy-right-container">
+                    <p>&copy; 2023 Agency</p>
+                    <p>A melhor agência web para seus projetos</p>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export { Footer }
